386e12834113b4fdf2d12cdfa80794bfbbdd85a0
deirus <deividas.rusenas@gmail.com>
2026-07-11 06:16:49 +0300

    P0a: ContainerEnums.cs — BUCKET_GROUP→BUCKET, added QUARANTINE status

--- Models/WarehouseModule/ContainerEnums.cs
+++ Models/WarehouseModule/ContainerEnums.cs
@@ -1,5 +1,5 @@
 namespace NordicBeesERP.Models.WarehouseModule;
 
-public enum ContainerType { BARREL, BUCKET_GROUP }
+public enum ContainerType { BARREL, BUCKET }
 
-public enum ContainerStatus { RECEIVED, IN_STOCK, RESERVED, IN_PRODUCTION, SOLD, RETURNED, WRITTEN_OFF }
+public enum ContainerStatus { RECEIVED, IN_STOCK, RESERVED, IN_PRODUCTION, QUARANTINE, SOLD, RETURNED, WRITTEN_OFF }
