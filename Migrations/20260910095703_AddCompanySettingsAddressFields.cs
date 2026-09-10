f47a9833b39dd206388aab96ddde103fa19c1144
deirus <deividas.rusenas@gmail.com>
2026-09-10 12:58:47 +0300

    feat: AddCompanySettingsAddressFields migration

---
+++ Migrations/20260910095703_AddCompanySettingsAddressFields.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NordicBeesERP.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCompanySettingsAddressFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "city",
+                table: "company_settings",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "postal_code",
+                table: "company_settings",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "country",
+                table: "company_settings",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "country_code",
+                table: "company_settings",
+                type: "varchar(2)",
+                maxLength: 2,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "city",
+                table: "company_settings");
+
+            migrationBuilder.DropColumn(
+                name: "postal_code",
+                table: "company_settings");
+
+            migrationBuilder.DropColumn(
+                name: "country",
+                table: "company_settings");
+
+            migrationBuilder.DropColumn(
+                name: "country_code",
+                table: "company_settings");
+        }
+    }
+}
