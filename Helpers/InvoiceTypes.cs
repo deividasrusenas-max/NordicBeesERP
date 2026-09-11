eb24cf6f9ba731783b58625435929879991e4f0c
deirus <deividas.rusenas@gmail.com>
2026-09-11 12:26:44 +0300

    feat: add InvoiceTypes constants for reverse-charge (96 str.) invoice type

---
+++ Helpers/InvoiceTypes.cs
@@ -0,0 +1,8 @@
+namespace NordicBeesERP.Helpers;
+
+public static class InvoiceTypes
+{
+    public const string Standard        = "PVM SĄSKAITA FAKTŪRA";
+    public const string Ulak6           = "6% PVM SĄSKAITA FAKTŪRA";
+    public const string ReverseCharge96 = "PVM SĄSKAITA FAKTŪRA (96 str.)";
+}
